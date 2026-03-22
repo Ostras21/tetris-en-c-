ab1109be93b9d0fd57d29a1c12a6772625197ee2
Ostrastr21 <oscar.trujillo@udea.edu.co>
2026-03-22 13:29:00 -0500

    Implementación completa de display.cpp: visualización fila por fila

--- src/display.cpp
+++ src/display.cpp
@@ -1,18 +1,53 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
+#include "display.h"
+#include <QTextStream>
 
-#include "board.h"
-#include "piece.h"
+Display::Display()  {}
+Display::~Display() {}
 
-class Display {
-public:
-    Display();
-    ~Display();
+void Display::imprimirTablero(const Tablero& tablero, const Pieza& pieza) const {
+    QTextStream salida(stdout);
+    uint16_t forma = pieza.getForma();
+    int posX = pieza.getPosX();
+    int posY = pieza.getPosY();
 
-    void imprimirTablero(const Tablero& tablero, const Pieza& piezaActual) const;
-    void imprimirMenu() const;
-    void imprimirPuntuacion(int puntuacion) const;
-    void imprimirGameOver() const;
-};
+    for (int y = 0; y < tablero.getAlto(); y++) {
+        for (int x = 0; x < tablero.getAncho(); x++) {
+            bool enTablero = tablero.celdaOcupada(x, y);
+            bool enPieza   = false;
 
-#endif // DISPLAY_H
+            int filaPieza = y - posY;
+            int colPieza  = x - posX;
+
+            if (filaPieza >= 0 && filaPieza < 4 && colPieza >= 0 && colPieza < 4) {
+                int bitIndex = 15 - (filaPieza * 4 + colPieza);
+                enPieza = (forma >> bitIndex) & 1;
+            }
+
+            if (enTablero || enPieza) {
+                salida << "# ";
+            } else {
+                salida << ". ";
+            }
+        }
+        salida << "\n";
+    }
+    salida.flush();
+}
+
+void Display::imprimirMenu() const {
+    QTextStream salida(stdout);
+    salida << "Accion: [A]Izq [D]Der [S]Bajar [W]Rotar [Q]Salir: ";
+    salida.flush();
+}
+
+void Display::imprimirPuntuacion(int puntuacion) const {
+    QTextStream salida(stdout);
+    salida << "Puntuacion: " << puntuacion << "\n";
+    salida.flush();
+}
+
+void Display::imprimirGameOver() const {
+    QTextStream salida(stdout);
+    salida << "\n--- GAME OVER ---\n";
+    salida.flush();
+}
